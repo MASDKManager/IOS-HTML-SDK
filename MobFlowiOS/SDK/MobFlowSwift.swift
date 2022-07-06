17c83b2fc09919680249e8ad5db10444b2e47d9d
Maarouf <maarouf.karakeh@mobileartsme.com>
2022-07-06 15:02:10 +0300

    1.2.9

--- MobFlowiOS/SDK/MobFlowSwift.swift
+++ MobFlowiOS/SDK/MobFlowSwift.swift
@@ -99,7 +99,7 @@ public class MobiFlowSwift: NSObject
             
             Adjust.appDidLaunch(adjustConfig)
              
-            let mob_sdk_version = "1.2.8"
+            let mob_sdk_version = "1.2.9"
             Adjust.addSessionCallbackParameter("mob_sdk_version", value: mob_sdk_version)
             Adjust.addSessionCallbackParameter("user_uuid", value: self.generateUserUUID())
             Adjust.addSessionCallbackParameter("Firebase_App_InstanceId", value: self.faid)
