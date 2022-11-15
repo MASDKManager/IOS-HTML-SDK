9550bae355ef58a9869598ca1106ab7ca70b2637
VishnuPalot <vishnu@mobiboxsoftech.com>
2022-11-15 11:34:23 +0530

    1.6.5

--- MobFlowiOS/SDK/MobFlowSwift.swift
+++ MobFlowiOS/SDK/MobFlowSwift.swift
@@ -8,7 +8,7 @@ import YandexMobileMetrica
 public class MobiFlowSwift: NSObject
 {
     
-    let mob_sdk_version = "1.6.6"
+    let mob_sdk_version = "1.6.5"
     var isAppmetrica = false
     var isDeeplinkURL = false
     var isUnityApp = false
